c839d86551ff0e376e759b2620fa3bb56c92c09a
simran112456 <simrannarang219@gmail.com>
2021-10-29 12:29:52 +0530

    devops infra ci cd2

---
+++ java/com/myapp/spring/DevopsInfrastructureApplication.java
@@ -0,0 +1,24 @@
+package com.myapp.spring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class DevopsInfrastructureApplication extends SpringBootServletInitializer {
+
+
+public static void main(String[] args) {
+SpringApplication.run(DevopsInfrastructureApplication.class, args);
+}
+
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+return builder.sources(DevopsInfrastructureApplication.class);
+}
+
+
+}
